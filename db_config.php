da1b873b11a08dab68cb203b456889369eb5982c
myrosmiyamoto <108709546+myrosmiyamoto@users.noreply.github.com>
2024-10-22 20:01:16 +0900

    [FIX] db_config.phpを修正

--- db_config.php
+++ db_config.php
@@ -1,7 +1,7 @@
 <?php
 // データベースの接続情報
-define( 'DB_HOST', 'database:3306');
-define( 'DB_USER', 'root');
-define( 'DB_PASS', 'tiger');
+define( 'DB_HOST', 'host:3306');
+define( 'DB_USER', 'user');
+define( 'DB_PASS', 'password');
 define( 'DB_NAME', 'board');
 ?>
