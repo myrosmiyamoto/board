8e7a46e2b54df761714c11d96260bd55a4a0ec0d
myrosmiyamoto <108709546+myrosmiyamoto@users.noreply.github.com>
2024-10-22 21:07:15 +0900

    [Update] db_config.php にわかりやすい説明を追加

--- db_config.php
+++ db_config.php
@@ -1,7 +1,9 @@
 <?php
 // データベースの接続情報
+// hostの部分を [DB インスタンスのエンドポイント] に変更する
 define( 'DB_HOST', 'host:3306');
-define( 'DB_USER', 'user');
-define( 'DB_PASS', 'password');
-define( 'DB_NAME', 'board');
+// DB に接続するためのユーザ名とパスワード、データベースを指定している
+define( 'DB_USER', 'board_admin');  // Webアプリからのデータベースアクセス専用のユーザを指定している
+define( 'DB_PASS', 'password');  // board_admin のパスワードを指定している
+define( 'DB_NAME', 'board');  // Webアプリから使用するデータベースを指定している
 ?>
