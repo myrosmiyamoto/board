393d22e7e1161baa5e2067e3a5edd881a7ddcd6a
myrosmiyamoto <108709546+myrosmiyamoto@users.noreply.github.com>
2026-01-21 10:41:26 +0900

    Update db_config.php
    データベースの接続先をローカルホストに変更

--- db_config.php
+++ db_config.php
@@ -1,7 +1,6 @@
 <?php
 // データベースの接続情報
-// hostの部分を [DB インスタンスのエンドポイント] に変更する
-define('DB_HOST', 'host:3306');
+define('DB_HOST', 'localhost:3306');
 // DB に接続するためのユーザ名とパスワード、データベースを指定している
 define('DB_USER', 'board_admin');  // Webアプリからのデータベースアクセス専用のユーザを指定している
 define('DB_PASS', 'password');  // board_admin のパスワードを指定している
